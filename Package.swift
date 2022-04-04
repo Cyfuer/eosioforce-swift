2a614ee73f89c46eea34cb35b70b6800d966c02f
Cai Han <2368498920@qq.com>
2022-04-04 12:38:36 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "eosio-swift",
+    products: [
+        .library(name: "EOSIO", targets: ["EOSIO"]),
+    ],
+    dependencies: [
+        .package(name: "secp256k1gm", url: "https://github.com/greymass/secp256k1.git", .branch("master")),
+        .package(name: "QueryStringCoder", url: "https://github.com/jnordberg/swift-query-string-coder.git", .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "EOSIO",
+            dependencies: ["secp256k1gm", "CCrypto", "QueryStringCoder"]
+        ),
+        .target(
+            name: "CCrypto"
+        ),
+        .testTarget(
+            name: "EOSIOTests",
+            dependencies: ["EOSIO"]
+        ),
+    ]
+)
